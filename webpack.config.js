a3ce58afee1870e1b29d3285b8134d0dee8a21eb
Vi <vika.miroshnikova2016@gmail.com>
2023-03-04 21:05:59 +0300

    Добавлен шрифт (создана папка, файл стилей) (был трабл с конфигом - пофикшен)

--- webpack.config.js
+++ webpack.config.js
@@ -27,13 +27,9 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ],
-    },
-},
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
