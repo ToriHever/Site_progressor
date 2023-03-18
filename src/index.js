9d62edbaad9388ca9b44699215d5b9643b15255e
Vi <vika.miroshnikova2016@gmail.com>
2023-03-18 22:04:15 +0300

    не помню что именно но точно подключен LESS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import './styles/styles.css'
-import json from './assets/json.json'
+import * as $ from 'jquery'
+import '@styles/styles.css'
+import '@styles/less.less'
+import json from './assets/json'
 import png from './assets/png.png'
+import csv from './assets/table/table.csv'
 
 const post = 2342342
 
+$('pre').html(post.toString())
+
 console.log(post)
-console.log('JSON:', json)
+console.log('csv:', csv)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+//const CopyWbpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
@@ -11,25 +13,69 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
-
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@styles': path.resolve(__dirname, 'src/styles')
+        }
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: false,
+            }
         }),
 
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+//        new CopyWebpackPlugin([
+//            {
+//                from: path.resolve(__dirname, 'src/assets/ico.ico'),
+//                to: path.resolve(__dirname, 'dist')
+//            }
+//        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }
+        ),
     ],
 
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+            },
+                    'css-loader'
+                ]
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 use: ['file-loader']
-            }
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+            },
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+
         ]
     }
 }
